728469f18da592e153b93fd76946a4921fc1fe6e
luanesouza <annie.usa25@gmail.com>
2023-07-07 14:38:20 -0400

    chore: adds tests and models

---
+++ app/models/playdate.rb
@@ -0,0 +1,8 @@
+class Playdate < ApplicationRecord
+    belongs_to :inviter, class_name: "User"
+    belongs_to :invitee, class_name: "User"
+
+    def self.create_playdate(invitee_id, inviter_id, date)
+        Playdate.create(invitee_id: invitee_id, inviter_id: inviter_id, date: date)
+    end
+end
