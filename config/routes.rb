ef5eb0cd16a05998c9cacd7cadf2b5f373254ebf
luanesouza <annie.usa25@gmail.com>
2023-07-07 14:38:31 -0400

    chore: creates new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   get "/users/:id/playdates", to: "users#user_playdates"
+  get "/users/:id/connections", to: "users#user_connections"
+  post "/users/:id/playdates/create", to: "users#create_playdate_if_connection"
+  post "/users/:id/connections/create", to: "users#create_connection"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
