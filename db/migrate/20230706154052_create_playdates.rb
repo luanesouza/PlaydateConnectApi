56e847014605450b44682b803666c482985b5097
luanesouza <annie.usa25@gmail.com>
2023-07-11 21:31:49 -0400

    chore: runs rubocop and adds active storage

--- db/migrate/20230706154052_create_playdates.rb
+++ db/migrate/20230706154052_create_playdates.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreatePlaydates < ActiveRecord::Migration[5.2]
   def change
     create_table :playdates do |t|
--- db/migrate/20230706154343_create_conections.rb
+++ db/migrate/20230706154343_create_conections.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 class CreateConections < ActiveRecord::Migration[5.2]
   def change
     create_table :connections do |t|
       t.string :followed_id
       t.string :follower_id
+      t.string :status
       t.date :date
 
       t.timestamps
