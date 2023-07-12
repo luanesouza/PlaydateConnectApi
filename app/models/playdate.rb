c576185eacd70887dbbd39525ca7fa7fbe80d8af
luanesouza <annie.usa25@gmail.com>
2023-07-11 21:31:01 -0400

    chore: adds create method

--- app/models/playdate.rb
+++ app/models/playdate.rb
@@ -1,8 +1,10 @@
-class Playdate < ApplicationRecord
-    belongs_to :inviter, class_name: "User"
-    belongs_to :invitee, class_name: "User"
+# frozen_string_literal: true
 
-    def self.create_playdate(invitee_id, inviter_id, date)
-        Playdate.create(invitee_id: invitee_id, inviter_id: inviter_id, date: date)
-    end
+class Playdate < ApplicationRecord
+  belongs_to :inviter, class_name: 'User'
+  belongs_to :invitee, class_name: 'User'
+
+  def self.create_playdate(invitee_id, inviter_id, date)
+    Playdate.create(invitee_id: invitee_id, inviter_id: inviter_id, date: date)
+  end
 end
