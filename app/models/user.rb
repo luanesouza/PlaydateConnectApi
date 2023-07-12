275ec9b172fb95e632f942e3bc10746a2c42ab5f
luanesouza <annie.usa25@gmail.com>
2023-07-11 21:26:45 -0400

    feat: adds relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,24 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    has_many :playdates, foreign_key: 'invitee_id', dependent: :destroy
-    has_many :invited_playdates, class_name: 'Playdate', foreign_key: 'inviter_id', dependent: :destroy
-   
-    #Connection
-    has_many :followers, foreign_key: :follower_id , class_name: "Connection"
-    has_many :followed, through: :followers
-    
-    has_many :followed, foreign_key: :followed_id, class_name: "Connection"
-    has_many :followers, through: :followed
+  has_one_attached :avatar
+  validates :name, presence: true
+  validates :email, uniqueness: true
 
-    #Playdate
-    has_many :inviters, foreign_key: :inviter_id, class_name: "Playdate"
-    has_many :invited, through: :inviters
+  has_many :playdates, foreign_key: 'invitee_id', dependent: :destroy
+  has_many :invited_playdates, class_name: 'Playdate', foreign_key: 'inviter_id', dependent: :destroy
 
-    has_many :invited, foreign_key: :invited_id, class_name: "Playdate"
-    has_many :inviters, through: :invited
+  # Connection
+  has_many :followers, foreign_key: :follower_id, class_name: 'Connection'
+  has_many :followed, through: :followers
+
+  has_many :followed, foreign_key: :followed_id, class_name: 'Connection'
+  has_many :followers, through: :followed
+
+  # Playdate
+  has_many :inviters, foreign_key: :inviter_id, class_name: 'Playdate'
+  has_many :invited, through: :inviters
+
+  has_many :invited, foreign_key: :invited_id, class_name: 'Playdate'
+  has_many :inviters, through: :invited
 end
