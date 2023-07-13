06a59efc4eb018408c1b163d2132acc6bba10cb6
luanesouza <annie.usa25@gmail.com>
2023-07-13 17:57:30 -0400

    chore: adds playdate controller

--- app/controllers/playdate_controller.rb
+++ app/controllers/playdate_controller.rb
@@ -1,19 +1,12 @@
+# frozen_string_literal: true
+
 class PlaydateController < ApplicationController
+  
 
-    def self.find_playdates(user_id)
-      sanitized_id = user_id.to_i
+  private
 
-      if sanitized_id != 0
-        @playdates = self.all.where(invitee_id: sanitized_id) + Playdate.all.where(inviter_id: sanitized_id)
-        render json: @playdates
-      else
-        raise "Id must be a number"
-      end
-    end
-
-    private
-        # Only allow a trusted parameter "allow list" through.
-        def playground_params
-            params.require(:playground).permit(:inviter_id, :invitee_id, :date)
-        end
+  # Only allow a trusted parameter "allow list" through.
+  def playground_params
+    params.require(:playground).permit(:inviter_id, :invitee_id, :date)
+  end
 end
